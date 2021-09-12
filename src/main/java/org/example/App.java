3884d04f32c34d315b6ef7993297b87469f8718e
Luis Curtiellas <curtiellas98@gmail.com>
2021-09-11 22:51:47 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,32 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 luis curtiellas
+ */
+
+import java.util.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+
+        //the password key to which attempts are compared
+        final String password = "abc$123";
+
+        //scanning password attempt from user
+        System.out.println("What is the password? ");
+        String attempt = sc.nextLine();
+
+        //returns 0 if they are same
+        if (password.compareTo(attempt) == 0)
+            System.out.println("Welcome!\n");
+
+        //not match
+        else
+            System.out.println("I don't know you.\n");
+
+    }
+}
